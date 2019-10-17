aadc94442ca1dafcfa47fa4842e2d8d0bf18646e
AllenWu1985 <272016988@qq.com>
2019-10-17 08:52:45 +0800

    This is a refactor

--- coding/alien_invasion.py
+++ coding/alien_invasion.py
@@ -1,35 +1,23 @@
 import sys
 import pygame
-
-class AlienInvasion:
-    """This is the overall class to manage game assets and behavior"""
-
-    def __init__(self):
-        """Initalize the game and creat game resource."""
-        pygame.init()
-
-        self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien Invasion")
-
-        # Set the background color
-        self.bg_color=(230,230,230)
-
-    def run_game(self):
-        """ Start the main loop for the game """
-        while True:
-            # Watch for mouse and key board event
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-
-if __name__=='__main__':
-    #Make game instance and run the game.
-    ai = AlienInvasion()
-    ai.run_game()
+from settings import Settings
+ 
+def run_game(): 
+    # 初始化pygame、设置和屏幕对象 
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) 
+    pygame.display.set_caption("Alien Invasion")
+ 
+    # 开始游戏主循环 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # 每次循环时都重绘屏幕 
+        screen.fill(ai_settings.bg_color) 
+                 
+        # 让最近绘制的屏幕可见 
+        pygame.display.flip()
+run_game()
 
